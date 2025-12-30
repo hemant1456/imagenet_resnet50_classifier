d57766e708dfc90b465ed4215b3b2fd25850b341
hemant1456 <hkbhambhu@gmail.com>
2025-12-30 16:48:50 +0530

    added transformation like mixup, random affine

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,24 +1,34 @@
 import torchvision
 import torch
 from torch.utils.data import DataLoader
-from torchvision import transforms
+from torchvision.transforms import v2
 
 mean = (0.4914, 0.4822, 0.4465)
 std  = (0.2470, 0.2435, 0.2616)
 
-transformations = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean,std)])
+train_transformations = v2.Compose([
+    v2.RandomHorizontalFlip(p=0.2),
+    v2.RandomApply([v2.RandomAffine(degrees=15,translate=(0.1,0.1),scale=(0.75,1.25),shear=10)],p=0.2),
+    v2.ToImage(),
+    v2.ToDtype(torch.float32,scale=True),
+    v2.Normalize(mean,std)])
+
+test_transformations = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32,scale=True),
+    v2.Normalize(mean,std)])
 
 
-def cifar_10_dataloader():
-    train_dataset = torchvision.datasets.CIFAR10(root = './cifar10_data',train=True, transform=transformations, target_transform=None, download=True)
-    test_dataset = torchvision.datasets.CIFAR10(root = './cifar10_data',train=False, transform=transformations, target_transform=None, download=True)
+def cifar_10_dataloader(batch_size=16):
+    train_dataset = torchvision.datasets.CIFAR10(root = './cifar10_data',train=True, transform=train_transformations, target_transform=None, download=True)
+    test_dataset = torchvision.datasets.CIFAR10(root = './cifar10_data',train=False, transform=test_transformations, target_transform=None, download=True)
 
     print(f" \nthe class are   {'--'.join(train_dataset.classes)}\n")
 
     print(f"{train_dataset.data.shape=}")
     print(f"{test_dataset.data.shape=}")
-    train_loader = DataLoader(dataset = train_dataset, batch_size=16, shuffle=True, pin_memory=False,num_workers=3)
-    test_loader = DataLoader(dataset = test_dataset, batch_size=16, shuffle=True, pin_memory=False)
+    train_loader = DataLoader(dataset = train_dataset, batch_size=batch_size, shuffle=True, pin_memory=False,num_workers=3)
+    test_loader = DataLoader(dataset = test_dataset, batch_size=batch_size, shuffle=True, pin_memory=False)
 
     return train_dataset, test_dataset, train_loader, test_loader
 
