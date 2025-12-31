732f33f31ed881d610881badf165041cba5ef7c2
hemant1456 <hkbhambhu@gmail.com>
2025-12-31 18:07:07 +0530

    added DDP distributed loader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -3,6 +3,7 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision.transforms import v2
 from tinyimagenet import TinyImageNet
+from torch.utils.data.distributed import DistributedSampler
 
 cifar_mean = (0.4914, 0.4822, 0.4465)
 cifar_std  = (0.2470, 0.2435, 0.2616)
@@ -35,8 +36,8 @@ def cifar_10_dataloader(batch_size=16):
 
 
 
-cifar_mean = (0.480, 0.448, 0.398)
-cifar_std  = (0.276, 0.269, 0.282)
+tiny_imagenent_mean = (0.480, 0.448, 0.398)
+tiny_imagenent_std  = (0.276, 0.269, 0.282)
 tiny_imagenet_train_transformations = v2.Compose([
     v2.RandomHorizontalFlip(p=0.2),
     v2.RandomApply([v2.RandomAffine(degrees=15,translate=(0.1,0.1),scale=(0.75,1.25),shear=10)],p=0.2),
@@ -47,15 +48,20 @@ tiny_imagenet_train_transformations = v2.Compose([
 tiny_imagenet_test_transformations = v2.Compose([
     v2.ToImage(),
     v2.ToDtype(torch.float32,scale=True),
-    v2.Normalize(cifar_mean,cifar_std)])
+    v2.Normalize(tiny_imagenent_mean,tiny_imagenent_std)])
 
-def tiny_imagenet_dataloader(batch_size=16):
+def tiny_imagenet_dataloader(batch_size=16,distributed=False):
     train_dataset = TinyImageNet(root='./tiny_imagenet_data',split='train',transform=tiny_imagenet_train_transformations)
     test_dataset = TinyImageNet(root='./tiny_imagenet_data',split='val',transform=tiny_imagenet_test_transformations)
-    train_loader = DataLoader(dataset = train_dataset, batch_size=batch_size, shuffle=True, pin_memory=False,num_workers=0)
+
+    data_sampler=None
+    if distributed:
+        data_sampler = DistributedSampler(train_dataset)
+
+    train_loader = DataLoader(dataset = train_dataset, batch_size=batch_size, shuffle=True, pin_memory=False,num_workers=0,sampler=data_sampler,drop_last=True)
     test_loader = DataLoader(dataset = test_dataset, batch_size=batch_size, shuffle=True, pin_memory=False)
 
-    return train_dataset, test_dataset, train_loader, test_loader
+    return train_dataset, test_dataset, train_loader, test_loader, data_sampler
 
 
 
